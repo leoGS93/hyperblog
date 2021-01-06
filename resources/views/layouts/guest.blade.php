385b51cd26c584ba1285df4517be563457ca8e3d
krmolinamolina <krmolinamolina@gmail.com>
2021-01-06 16:46:23 -0500

    inicio

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,4 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -11,31 +11,16 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
 
         <!-- Scripts -->
         {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script> --}}
-    {{-- </head>
+    </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
     </body>
-</html> --}} --}}
-@extends('../layoutP/base')
+</html>
 
-@section('body')
-    <body class="app">
-        @yield('content')
-        @include('../layoutP/components/dark-mode-switcher')
-
-        <!-- BEGIN: JS Assets-->
-        <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places"></script>
-        <script src="{{ mix('dist/js/app.js') }}"></script>
-        <!-- END: JS Assets-->
-
-        @yield('script')
-    </body>
-@endsection
 
